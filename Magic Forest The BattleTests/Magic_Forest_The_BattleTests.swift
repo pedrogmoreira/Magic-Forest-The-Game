7512bf96a656b931b5f3e5a8c945b52786214ccc
Pedro Henrique <pedrogmoreira93@hotmail.com>
2015-10-19 15:19:33 -0200

    Initial Commit

---
+++ Magic Forest The BattleTests/Magic_Forest_The_BattleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Magic_Forest_The_BattleTests.swift
+//  Magic Forest The BattleTests
+//
+//  Created by Pedro Henrique Goncalves Moreira on 19/10/15.
+//  Copyright © 2015 Team Magic Forest. All rights reserved.
+//
+
+import XCTest
+@testable import Magic_Forest_The_Battle
+
+class Magic_Forest_The_BattleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
