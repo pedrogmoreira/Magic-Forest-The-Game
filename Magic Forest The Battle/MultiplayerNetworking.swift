c51e2151adeb57cebad868ae4ab4b339d09322c3
Marcelo <marceloabk@hotmail.com>
2015-11-11 18:03:23 -0200

    Creating MultiplayerNetworking class
    Starting to create allRandomNumbersAreReceived method

---
+++ Magic Forest The Battle/MultiplayerNetworking.swift
@@ -0,0 +1,202 @@
+//
+//  MultiplayerNetworking.swift
+//  Magic Forest The Battle
+//
+//  Created by Marcelo Cristiano Araujo Silva on 11/11/15.
+//  Copyright © 2015 Team Magic Forest. All rights reserved.
+//
+
+import Foundation
+import GameKit
+
+
+protocol MultiplayerProtocol {
+    func matchEnded()
+}
+
+enum GameState: Int {
+    case WaintingForMatch, WaintingForRandomNumber, WaitingForStart, Playing, Done
+}
+
+enum MessageType: Int {
+    case RandomNumber, GameBegin, GameOver
+}
+
+struct Message {
+    let messageType: MessageType
+}
+
+struct MessageRandomNumber {
+    let message: Message
+    let randomNumber: UInt32
+}
+
+struct MessageGameBegin {
+    let message: Message
+}
+
+struct MessageGameOver {
+    let message: Message
+}
+
+class MultiplayerNetworking: NSObject, GameKitHelperDelegate {
+    var delegate: MultiplayerProtocol?
+    var ourRandomNumber: UInt32 = 0
+    var gameState: GameState
+    var isPlayer1: Bool
+    var receivedAllRandomNumber: Bool
+    var orderOfPlayers: [RandomNumberDetails] = []
+    
+    override init() {
+        ourRandomNumber = arc4random()
+        gameState = GameState.WaintingForMatch
+        isPlayer1 = false
+        receivedAllRandomNumber = false
+        
+        orderOfPlayers = [RandomNumberDetails]()
+        
+        orderOfPlayers.append(RandomNumberDetails(playerId: GKLocalPlayer.localPlayer().playerID!, randomNumber: ourRandomNumber))
+        
+        super.init()
+    }
+    
+    class RandomNumberDetails: NSObject {
+        let playerId: String
+        let randomNumber: UInt32
+        
+        init(playerId: String, randomNumber: UInt32) {
+            self.playerId = playerId
+            self.randomNumber = randomNumber
+            super.init()
+        }
+        
+        override func isEqual(object: AnyObject?) -> Bool {
+            let randomNumberDetails = object as? RandomNumberDetails
+            return randomNumberDetails?.playerId == self.playerId
+        }
+    }
+    
+    func matchStarted() {
+        print("Match has started successfuly")
+        if receivedAllRandomNumber {
+            gameState = GameState.WaitingForStart
+        } else {
+            gameState = GameState.WaintingForRandomNumber
+        }
+        
+        sendRandomNumber()
+        tryStartGame()
+    }
+    
+    func sendData(data: NSData) {
+        let gameKitHelper = GameKitHelper.sharedInstance
+        
+        if let multiplayerMatch = gameKitHelper.multiplayerMatch {
+            do {
+                try multiplayerMatch.sendDataToAllPlayers(data, withDataMode: .Reliable)
+            } catch let error as NSError {
+                print("Error:\(error.localizedDescription)")
+                matchEnded()
+            }
+        }
+    }
+    
+    func sendRandomNumber() {
+        var message = MessageRandomNumber(message: Message(messageType: MessageType.RandomNumber), randomNumber: ourRandomNumber)
+        
+        let data = NSData(bytes: &message, length: sizeof(MessageRandomNumber))
+        sendData(data)
+    }
+    
+    func sendBeginGame() {
+        var message = MessageGameBegin(message: Message(messageType: MessageType.GameBegin))
+        let data = NSData(bytes: &message, length: sizeof(MessageGameBegin))
+        sendData(data)
+    }
+    
+    func tryStartGame() {
+        if isPlayer1 && gameState == GameState.WaitingForStart {
+            gameState = GameState.Playing
+            sendBeginGame()
+        }
+    }
+    
+    func processReceivedRandomNumber(randomNumberDetails: RandomNumberDetails) {
+        let multableArray = NSMutableArray(array: orderOfPlayers)
+        multableArray.addObject(randomNumberDetails)
+        
+        let sortByRandomNumber = NSSortDescriptor(key: "randomNumber", ascending: false)
+        let sortDescriptiors = [sortByRandomNumber]
+        multableArray.sortUsingDescriptors(sortDescriptiors)
+        
+        orderOfPlayers = NSArray(array: multableArray) as! [RandomNumberDetails]
+        
+        if allRandomNumbersAreReceived() {
+            receivedAllRandomNumber = true
+        }
+    }
+    
+    func isLocalPlayer1() -> Bool {
+        return false
+    }
+    
+    func matchEnded() {
+        delegate?.matchEnded()
+    }
+    
+    func allRandomNumbersAreReceived() -> Bool {
+//        var receivedRandomNumbers = Set<UInt32>()
+//        
+//        for playerDetail
+//        
+        return false
+    }
+    
+    func matchReceivedData(match: GKMatch, data: NSData, fromPlayer player: String) {
+        let message = UnsafePointer<Message>(data.bytes).memory
+        
+        if message.messageType == MessageType.RandomNumber {
+            let messageRandomNumber = UnsafePointer<MessageRandomNumber>(data.bytes).memory
+        
+            print("Received random number:\(messageRandomNumber.randomNumber)")
+            
+            var tie = false
+            if messageRandomNumber.randomNumber == ourRandomNumber {
+                print("Tie")
+                tie = true
+                
+                var idx: Int?
+                
+                for (index, randomNumberDetails) in orderOfPlayers.enumerate() {
+                    if randomNumberDetails.randomNumber == ourRandomNumber {
+                        idx = index
+                        break
+                    }
+                }
+                
+                if let validIndex = idx {
+                    ourRandomNumber = arc4random()
+                    orderOfPlayers.removeAtIndex(validIndex)
+                    orderOfPlayers.append(RandomNumberDetails(playerId: GKLocalPlayer.localPlayer().playerID!, randomNumber: ourRandomNumber))
+                }
+                
+                sendRandomNumber()
+            } else {
+                processReceivedRandomNumber(RandomNumberDetails(playerId: player, randomNumber: messageRandomNumber.randomNumber))
+            }
+            
+            if receivedAllRandomNumber {
+                isPlayer1 = isLocalPlayer1()
+            }
+            
+            if !tie && receivedAllRandomNumber {
+                if gameState == GameState.WaintingForRandomNumber {
+                    gameState = GameState.WaitingForStart
+                }
+                tryStartGame()
+            }
+        }
+    }
+    
+    
+}
